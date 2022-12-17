ca0e193105288079817b62e2dc86a5e8b0819fb9
Lilthrasher <ghostlysmith@gmail.com>
2022-12-17 12:11:23 -0800

    Rewrite, Added console output to tkinter text box.

--- discordi.py
+++ discordi.py
@@ -1,81 +1,182 @@
 import os
+import threading
 import customtkinter as tkinter
 import ffmpeg
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import asksaveasfilename
 
-padX = 20
-padY = 20
+padX = 15
+padY = 15
 
 class App(tkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        #not the best way?
-        global path, outpath, resMethod, resList, resSelected, state, Xvar, Yvar, presetframe, resframe, presetframe
+        global inpath, outpath, resOption, Xvar, Yvar
 
         self.title("Discordipy")
-        self.geometry("1000x500")
-        self.minsize(1000,500)
+        self.geometry("1000x800")
+        self.resizable(False, False)
+        self.grid_rowconfigure(5, weight=1)
+        self.grid_columnconfigure([0,1,2,3,4,5], weight=1)
 
-        self.grid_rowconfigure([0,1,2,3], weight=1)
-        self.grid_columnconfigure(0, weight=1)
-
-        path = tkinter.StringVar()
-        path.set("")
-        outpath = tkinter.StringVar()
+        inpath= tkinter.StringVar()
+        inpath.set("")
+        outpath= tkinter.StringVar()
         outpath.set("")
-        resMethod = tkinter.IntVar()
-        resMethod.set(1)
-        resList = ["1080p", "720p", "480p", "360p", "240p", "144p"]
-        resSelected = tkinter.StringVar()
-        state = tkinter.StringVar()
-        state.set("disabled")
+        resOption = tkinter.IntVar()
+        resOption.set(1)
         Xvar = tkinter.IntVar()
         Xvar.set(0)
         Yvar = tkinter.IntVar()
         Yvar.set(0)
 
-        #Input Frame
-        self.inputfileframe = InputFileFrame(self, header_name="InputFileFrame")
-        self.inputfileframe.grid(padx=padX, pady=10, row=0, column=0, columnspan=3, sticky="new")
+        self.fileframe = FileFrame(self, header_name="FileFrame")
+        self.fileframe.grid(padx=padX, pady=padY, column=0, columnspan=6, row=0, sticky="ew")
 
-        #Preset Frame
-        presetframe = PresetFrame(self, header_name="PresetFrame")
-        presetframe.grid(padx=padX, pady=10, row=1, column=0)
+        self.nochangeframe = NoChangeFrame(self, header_name="NoChangeFrame")
+        self.nochangeframe.grid(padx=padX, pady=padY, column=2, row=1)
 
-        #Resolution Frame
-        resframe = ResolutionFrame(self, header_name="ResolutionFrame")
-        resframe.grid(padx=padX, pady=10, row=2, column=0)
+        self.presetframe = PresetFrame(self, header_name="PresetFrame")
+        self.presetframe.grid(padx=0, pady=padY, column=3, row=1)
 
-        #Save Frame
-        self.savefileframe = SaveFileFrame(self, header_name="SaveFileFrame")
-        self.savefileframe.grid(padx=padX, pady=10, row=3, column=0, columnspan=3, sticky="sew")
+        self.resolutionframe = ResolutionFrame(self, header_name="ResolutionFrame")
+        self.resolutionframe.grid(padx=0, pady=padY, columnspan=6, column=0, row=2)
 
-        #Discordify Button
-        self.discordify = Discordify(self, header_name="Discordify")
-        self.discordify.grid(padx=padX, pady=10, row=4, column=0, columnspan=3)
+        self.saveframe = SaveFrame(self, header_name="SaveFrame")
+        self.saveframe.grid(padx=padX, pady=padY, column=0, columnspan=6, row=3, sticky="ew")
+
+        self.buttonframe = ButtonFrame(self, header_name="ButtonFrame")
+        self.buttonframe.grid(padx=padX, pady=padY, column=0, columnspan=6, row=4)
+
+        self.consoleframe = ConsoleFrame(self, header_name="ConsoleFrame")
+        self.consoleframe.grid(padx=padX, pady=padY, column=0, columnspan=6, row=5, sticky="news")
+
+
+class FileFrame(tkinter.CTkFrame):
+    def __init__(self, *args, header_name="FileFrame", **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.columnconfigure(1, weight=1)
+
+        self.filelabel = tkinter.CTkLabel(self, text="Input File:")
+        self.filelabel.grid(padx=padX, pady=padY, column=0, row=0)
+
+        self.filepathlabel = tkinter.CTkLabel(self, textvariable=inpath)
+        self.filepathlabel.grid(padx=padX, pady=padY, column=1, row=0)
+
+        self.pickfilebtn = tkinter.CTkButton(self, text="Select File/Folder", command=Discordipy.fileselect)
+        self.pickfilebtn.grid(padx=padX, pady=padY, column=2, row=0)
+
+
+class NoChangeFrame(tkinter.CTkFrame):
+    def __init__(self, *args, header_name="NoChangeFrame", **kwargs):
+        super().__init__(*args, **kwargs)
+
+        nochange = tkinter.CTkRadioButton(self, variable=resOption, value=1, command=Discordipy.radiostate, text="     Keep Resolution", height=29, width=150)
+        nochange.grid(padx=padX, pady=padY, column=0, columnspan=2, row=0)
+
+
+class PresetFrame(tkinter.CTkFrame):
+    def __init__(self, *args, header_name="PresetFrame", **kwargs):
+        super().__init__(*args, **kwargs)
+
+        global listSelection, presetcombo
+
+        self.preset = tkinter.CTkRadioButton(self, variable=resOption, value=2, command=Discordipy.radiostate, text="     Preset:")
+        self.preset.grid(padx=padX, pady=padY, column=2, row=0)
+
+        comboList = ["1080p", "720p", "480p", "360p", "240p", "144p"]
+        listSelection = tkinter.StringVar()
+        listSelection.set("1080p")
+
+        presetcombo = tkinter.CTkComboBox(self, justify="center", values=comboList, variable=listSelection, state="disabled")
+        presetcombo.grid(padx=padX, pady=padY, column=3, row=0)
+
+
+class ResolutionFrame(tkinter.CTkFrame):
+    def __init__(self, *args, header_name="ResolutionFrame", **kwargs):
+        super().__init__(*args, **kwargs)
+
+        global customX, customY
+
+        custom = tkinter.CTkRadioButton(self, variable=resOption, value=3, command=Discordipy.radiostate, text="     Resolution:")
+        custom.grid(padx=padX, pady=padY, column=0, row=1)
+
+        customX = tkinter.CTkEntry(self, state="disabled", textvariable=Xvar)
+        customX.grid(padx=padX, pady=padY, column=1, row=1)
+
+        self.X = tkinter.CTkLabel(self, text="X")
+        self.X.grid(padx=0, pady=0, column=2, row=1)
+
+        customY = tkinter.CTkEntry(self, state="disabled", textvariable=Yvar)
+        customY.grid(padx=padX, pady=padY, column=3, row=1)
+
+        
+class SaveFrame(tkinter.CTkFrame):
+    def __init__(self, *args, header_name="SaveFrame", **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.columnconfigure(1, weight=1)
+
+        self.savelabel = tkinter.CTkLabel(self, text="Save As:")
+        self.savelabel.grid(padx=padX, pady=padY, column=0, row=0)
+
+        self.savepathlabel = tkinter.CTkLabel(self, textvariable=outpath)
+        self.savepathlabel.grid(padx=padX, pady=padY, column=1, row=0, sticky="ew")
+
+        self.savefilebtn = tkinter.CTkButton(self, text="Browse", command=Discordipy.savefileselect)
+        self.savefilebtn.grid(padx=padX, pady=padY, column=2, row=0)
+
+
+class ButtonFrame(tkinter.CTkFrame):
+    def __init__(self, *args, header_name="ButtonFrame", **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.button = tkinter.CTkButton(self, text="Discordipy", command=Discordipy.optionset, width=250, height=100)
+        self.button.grid(padx=0, pady=0, column=0, row=0)
+
+
+class ConsoleFrame(tkinter.CTkFrame):
+    def __init__(self, *args, header_name="ConsoleFrame", **kwargs):
+        super().__init__(*args, **kwargs)
+
+        global console
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        console = tkinter.CTkTextbox(self)
+        console.grid(padx=0, pady=0, column=0, row=0, sticky="news")
+
+
+class Discordipy:
+    def __init__(self):
+        pass
 
     def fileselect():
-        global vwidth, vheight, vlength, vbitrate, abitrate, newRate
+
+        global newRate, abitrate , vframes
+
         select = askopenfilename()
         if select:
-            path.set(select)
+            inpath.set(select)
             file_name, file_ext = os.path.splitext(select)
-            newName = (file_name+" - Discord"+file_ext)
+            newName = (file_name+" - Discordipy"+file_ext)
             outpath.set(newName)
             probe = ffmpeg.probe(select)
             video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
             audio_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'audio'), None)
             vwidth = int(video_stream["width"])
             vheight = int(video_stream["height"])
+            vframes = int(video_stream["nb_frames"])
             vlength = float(round((float(video_stream["duration"]))/60, 1))
-            vbitrate = int(video_stream["bit_rate"])
+            # vbitrate = int(video_stream["bit_rate"])
             abitrate = int(audio_stream["bit_rate"])
             Xvar.set(vwidth)
             Yvar.set(vheight)
             newRate = int((8/(vlength*0.0000075))-abitrate)
-            resMethod.set(1)
+            resOption.set(1)
         else:
             pass
 
@@ -87,20 +188,26 @@ class App(tkinter.CTk):
             pass
 
     def radiostate():
-        match resMethod.get():
+        match resOption.get():
             case 1:
-                state.set("disabled")
-                resframe.setres()
-                presetframe.setpreset()
+                presetcombo.configure(state="disabled")
+                customX.configure(state="disabled")
+                customY.configure(state="disabled")
             case 2:
-                state.set("normal")
-                resframe.setres()
-                presetframe.setpreset()
-
-    def discordify():
-        match resMethod.get():
+                presetcombo.configure(state="normal")
+                customX.configure(state="disabled")
+                customY.configure(state="disabled")
+            case 3:
+                presetcombo.configure(state="disabled")
+                customX.configure(state="normal")
+                customY.configure(state="normal")
+    
+    def optionset():
+        match resOption.get():
             case 1:
-                match resSelected.get():
+                pass
+            case 2:
+                match listSelection.get():
                     case "1080p":
                         Xvar.set(1920)
                         Yvar.set(1080)
@@ -119,109 +226,36 @@ class App(tkinter.CTk):
                     case "144p":
                         Xvar.set(256)
                         Yvar.set(144)
-                input_stream = ffmpeg.input(path.get())
-                video = input_stream.video.filter("scale", Xvar.get(), Yvar.get())
-                audio = input_stream.audio
-                concat = ffmpeg.concat(video, audio, v=1, a=1).node
-                video_out = concat[0]
-                audio_out = concat[1]
-                out = ffmpeg.output(video_out, audio_out, outpath.get(), video_bitrate=newRate, audio_bitrate=abitrate, format="mp4")
-                out.run()
-            case 2:
-                input_stream = ffmpeg.input(path.get())
-                video = input_stream.video.filter("scale", Xvar.get(), Yvar.get())
-                audio = input_stream.audio
-                concat = ffmpeg.concat(video, audio, v=1, a=1).node
-                video_out = concat[0]
-                audio_out = concat[1]
-                out = ffmpeg.output(video_out, audio_out, outpath.get(), video_bitrate=newRate, audio_bitrate=abitrate, format="mp4")
-                out.run()
+            case 3:
+                pass
+        Discordipy.threadstart()
 
-class InputFileFrame(tkinter.CTkFrame):
-    def __init__(self, *args, header_name="InputFileFrame", **kwargs):
-        super().__init__(*args, **kwargs)
+    def threadstart():
+        threading.Thread(target=Discordipy.runff).start()
 
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
+    def runff():
 
-        #File Input
-        self.file = tkinter.CTkLabel(master=self, text="Input File:")
-        self.file.grid(padx=padX, pady=padY, row=0, column=0)
+        global process
 
-        self.path = tkinter.CTkLabel(master=self, textvariable=path, width=500)
-        self.path.grid(padx=padX, pady=padY, row=0, column=1, sticky="ew")
+        input_stream = ffmpeg.input(inpath.get())
+        video = input_stream.video.filter("scale", Xvar.get(), Yvar.get())
+        audio = input_stream.audio
+        concat = ffmpeg.concat(video, audio, v=1, a=1).node
+        video_out = concat[0]
+        audio_out = concat[1]
+        ffout = ffmpeg.output(video_out, audio_out, outpath.get(), video_bitrate=newRate, audio_bitrate=abitrate, format="mp4")
+        process = ffout.run_async(pipe_stdout=True, pipe_stderr=True)
+        threading.Thread(target=Discordipy.consoleout).start()
+        process.stderr
 
-        self.pickfile = tkinter.CTkButton(master=self, text="Select File/Folder", command=App.fileselect)
-        self.pickfile.grid(padx=padX, pady=padY, row=0, column=2)
-
-class PresetFrame(tkinter.CTkFrame):
-    def __init__(self, *args, header_name="PresetFrame", **kwargs):
-        super().__init__(*args, **kwargs)
-
-        #Preset Select
-        self.radio = tkinter.CTkRadioButton(master=self, text="Preset:", variable=resMethod, value=1, command=App.radiostate)
-        self.radio.grid(padx=padX, pady=padY, row=2, column=0)
-
-        self.presetmenu = tkinter.CTkComboBox(master=self, state="normal", justify="center", values=resList, variable=resSelected)
-        self.presetmenu.grid(padx=padX, pady=padY, row=2, column=1)
-
-    def setpreset(self):
-        match state.get():
-            case "disabled":
-                self.presetmenu.configure(state="normal")
-            case "normal":
-                self.presetmenu.configure(state="disabled")
-
-class ResolutionFrame(tkinter.CTkFrame):
-    def __init__(self, *args, header_name="ResolutionFrame", **kwargs):
-        super().__init__(*args, **kwargs)
-
-        #Resolution
-        self.radio2 = tkinter.CTkRadioButton(master=self, text="Resolution:", variable=resMethod, value=2, command= App.radiostate)
-        self.radio2.grid(padx=padX, pady=padY, row=3, column=0)
-
-        self.resX = tkinter.CTkEntry(master=self, state="disabled", textvariable=Xvar)
-        self.resX.grid(padx=padX, pady=padY, row=3, column=1)
-
-        self.x = tkinter.CTkLabel(master=self, text="X")
-        self.x.grid(padx=0, pady=0, row=3, column=2)
-
-        self.resY = tkinter.CTkEntry(master=self, state="disabled", textvariable=Yvar)
-        self.resY.grid(padx=padX, pady=padY, row=3, column=3)
-
-    def setres(self):
-        match state.get():
-            case "disabled":
-                self.resX.configure(state="disabled")
-                self.resY.configure(state="disabled")
-            case "normal":
-                self.resX.configure(state="normal")
-                self.resY.configure(state="normal")
-
-class SaveFileFrame(tkinter.CTkFrame):
-    def __init__(self, *args, header_name="SaveFileFrame", **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-
-        #File Save
-        self.outfile = tkinter.CTkLabel(master=self, text="Save As:")
-        self.outfile.grid(padx=padX, pady=padY, row=0, column=0)
-
-        self.outpath = tkinter.CTkLabel(master=self, textvariable=outpath, width=500)
-        self.outpath.grid(padx=padX, pady=padY, row=0, column=1, sticky="ew")
-
-        self.pickoutfile = tkinter.CTkButton(master=self, text="Browse", command=App.savefileselect)
-        self.pickoutfile.grid(padx=padX, pady=padY, row=0, column=2)
-
-class Discordify(tkinter.CTkFrame):
-    def __init__(self, *args, header_name="Discordify", **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.savebutton = tkinter.CTkButton(master=self, text="Discordify", command=App.discordify, width=250, height=100)
-        self.savebutton.grid(padx=0, pady=0, row=0, column=0)
-
+    def consoleout():
+        while True:
+            stderr = process.stderr.readline()
+            if not (stderr):
+                console.insert(tkinter.END, "Finished Discordipying your video!")
+                break
+            console.insert(tkinter.END, stderr)
+            console.see(tkinter.END)
 
 if __name__ == "__main__":
     app = App()
